7296430217896f35a313a7891e26954971c5f727
nandrepo1 <github25u7@gmail.com>
2025-10-14 10:23:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nandrepo1/Aakaza",
+    "https://github.com/nandrepo1/brandedsparsh-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
